436d97b6a0f36e42bbb4e51ded4481954e765007
Nina <nina@system76-pc.localdomain>
2024-01-22 17:36:07 -0600

    rudimentary router and app setup

---
+++ src/Trackers.tsx
@@ -0,0 +1,3 @@
+export const Trackers = () => {
+    return <div>Trackers</div>
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {Transactions} from "./Transactions";
+import {Accounts} from "./Accounts";
+import {Trackers} from "./Trackers";
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App/>
+    },
+
+    {
+        path: "/accounts",
+        element: <Accounts/>,
+    },
+
+    {
+        path: "/transactions/:accountId",
+        element: <Transactions/>,
+    },
+
+    {
+        path: "/trackers",
+        element: <Trackers/>,
+    },
+])
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
